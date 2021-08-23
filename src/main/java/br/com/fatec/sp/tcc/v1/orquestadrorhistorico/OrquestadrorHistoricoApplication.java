60e583a80283bbeb4f20ddcd11799c5d532cc1cb
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-23 14:11:58 -0300

    Inicializando projeto.

---
+++ src/main/java/br/com/fatec/sp/tcc/v1/orquestadrorhistorico/OrquestadrorHistoricoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestadrorhistorico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestadrorHistoricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestadrorHistoricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/sp/tcc/v1/orquestadrorhistorico/OrquestadrorHistoricoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestadrorhistorico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestadrorHistoricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
