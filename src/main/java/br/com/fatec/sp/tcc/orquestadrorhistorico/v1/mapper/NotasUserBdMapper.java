28a3b5f204141304c127d86e7a12d44cb6a45751
nascito_nicholas <nascito.nicholas@gmail.com>
2021-10-24 18:04:10 -0300

    Funcionalidade de historico por usuário.

--- src/main/java/br/com/fatec/sp/tcc/orquestadrorhistorico/v1/mapper/NotasUserBdMapper.java
+++ src/main/java/br/com/fatec/sp/tcc/orquestadrorhistorico/v1/mapper/NotasUserBdMapper.java
@@ -1,5 +1,6 @@
 package br.com.fatec.sp.tcc.orquestadrorhistorico.v1.mapper;
 
+import br.com.fatec.sp.tcc.orquestadrorhistorico.v1.controller.response.HistoricoResponse;
 import br.com.fatec.sp.tcc.orquestadrorhistorico.v1.controller.response.NotasResponse;
 import br.com.fatec.sp.tcc.orquestadrorhistorico.v1.integracao.orquestradoBd.response.Materia;
 import br.com.fatec.sp.tcc.orquestadrorhistorico.v1.integracao.orquestradoBd.response.NotaUsuario;
@@ -16,5 +17,13 @@ public interface NotasUserBdMapper {
             @Mapping(target = "professor", expression = "java(materia.getProfessores().get(0).getNome())"),
     })
     NotasResponse mapNotaMateriaBdToNotasResponse(final NotaUsuario nota, final Materia materia);
+
+    @Mappings({
+            @Mapping(target = "disciplina", source = "materia.codMateria"),
+            @Mapping(target = "nomeDisciplina", source = "materia.nome"),
+            @Mapping(target = "conclusao", source = "nota.anoMesConclusao"),
+            @Mapping(target = "media", source = "nota.notaFinal"),
+    })
+    HistoricoResponse mapNotaMateriaBdToHistoricoResponse(final NotaUsuario nota, final Materia materia);
 
 }
